881ce54f004feaa29eb608a67cbd416107183fec
Yeshey <41551785+Yeshey@users.noreply.github.com>
2021-10-20 02:46:35 +0100

    update decoder

--- TheThingsUno_Arduino/Final/decodeUplink.js
+++ TheThingsUno_Arduino/Final/decodeUplink.js
@@ -1,11 +1,13 @@
 function decodeUplink(input) {
-    return {
-      data: {
-        byte_moistsensor: input.bytes[0],
-        byte_dhttemperature: input.bytes[1],
-        byte_dhthumidity: input.bytes[2]
-      },
-      warnings: [],
-      errors: []
-    };
-  }
+  return {
+    data: {
+      byte_moistsensor: input.bytes[0],
+      byte_dht_hum_int: input.bytes[1],
+      byte_dht_hum_deci: input.bytes[2],
+      byte_dht_temp_int: input.bytes[3],
+      byte_dht_temp_deci: input.bytes[4]
+    },
+    warnings: [],
+    errors: []
+  };
+}
